cdb258f7419b1d7c8b95edb213ddb4e0725540c8
vijay karena <vijaykarena360@gmail.com>
2026-07-10 10:35:30 +0530

    mongoose schema methods : getJWT, validatePassword

--- src/app.js
+++ src/app.js
@@ -45,12 +45,10 @@ app.post("/login", async (req, res) => {
 
     if (!user) throw new Error("Invalid credentials!");
 
-    const isPasswordValid = await bcrypt.compare(password, user.password);
+    const isPasswordValid = await user.validatePassword(password);
     if (!isPasswordValid) throw new Error("Invalid credentials!");
 
-    const token = await jwt.sign({ _id: user._id }, "secret", {
-      expiresIn: "1d",
-    });
+    const token = await user.getJWT();
 
     res.cookie("token", token, {
       expires: new Date(Date.now() + 12 * 3600000),
