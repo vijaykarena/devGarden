fa0b66f84920dc999b7df834ebb5cdda35995d22
vijay karena <vijaykarena360@gmail.com>
2026-07-12 20:59:42 +0530

    connection request schema, connection request api, compound index

--- apiList.md
+++ apiList.md
@@ -11,8 +11,8 @@
 - PATCH /profile/password
 
 ## connectionRequestRouter
-- POST /request/send/interested/:userId
-- POST /request/send/ignored/:userId
+- POST /request/send/:status/:userId
+<!-- - POST /request/send/ignored/:userId -->
 - POST /request/review/accepted/:requestId
 - POST /request/review/rejected/:requestId
 
--- src/routes/request.js
+++ src/routes/request.js
@@ -1,15 +1,47 @@
 const express = require("express");
 const { userAuth } = require("../middleware/auth");
+const ConnectionRequest = require("../models/connectionRequest");
+const User = require("../models/user");
 
 const requestRouter = express.Router();
 
 requestRouter.post(
-  "/sendConnection",
+  "/request/send/:status/:toUserId",
   userAuth,
   async (req, res) => {
     try {
-      const user = req.user;
-      res.send(user);
+      const fromUserId = req.user._id;
+      const toUserId = req.params.toUserId;
+      const status = req.params.status;
+
+      const allowedStatus = ["ignored", "interested"];
+      if (!allowedStatus.includes(status)) {
+        throw new Error("Invalid status type!");
+      }
+
+      if (fromUserId.equals(toUserId)) {
+        throw new Error("You can not send request to yourself!");
+      }
+
+      const toUser = await User.findById(toUserId);
+      if (!toUser) throw new Error("Request user not found!");
+
+      const checkDuplicate = await ConnectionRequest.findOne({
+        $or: [
+          { fromUserId, toUserId },
+          { fromUserId: toUserId, toUserId: fromUserId },
+        ],
+      });
+      if (checkDuplicate) throw new Error("request already exist!");
+
+      const connectionRequest = new ConnectionRequest({
+        fromUserId,
+        toUserId,
+        status,
+      });
+      const data = await connectionRequest.save();
+
+      res.json({ message: "connection request send successfully!", data });
     } catch (err) {
       res.status(400).send("ERROR : " + err.message);
     }
